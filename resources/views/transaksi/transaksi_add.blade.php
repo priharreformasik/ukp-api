3b6e5c2261df03e0038cacc244e327dddbaec294
priharreformasik <priharreformasi@gmail.com>
2019-12-07 22:54:43 +0700

    view transaksi

--- resources/views/transaksi/transaksi_add.blade.php
+++ resources/views/transaksi/transaksi_add.blade.php
@@ -1,3 +1,4 @@
+
 @extends('layout')
 
 @section('content-header')
@@ -28,28 +29,32 @@
             <div class="col-md-6 pl-5 pr-5" style="float:left">
               <div class="row pl-5 pr-5" style="margin-top: 5px;">
                   <div class="col-md-4">
-                      <p>ID Transaksi</p>
+                      <p>Tanggal</p>
                   </div>
                   <div class="col-md-8">
-                    <input type="text" class="form-control" name="nama" placeholder="Asesmen" value="{{old('nama')}}">
+                    <input type="date" class="form-control" name="tanggal" placeholder="tanggal" value="{{old('harga')}}">
                   </div>
-              </div>
+              </div> 
               <div class="row pl-5 pr-5" style="margin-top: 5px;">
                   <div class="col-md-4">
                       <p>Klien</p>
                   </div>
                   <div class="col-md-8">
-                    <input type="number" class="form-control" name="harga" placeholder="Harga" value="{{old('harga')}}">
+                    <select class="form-control select2" name="klien" placeholder="Pilih Asesmen/Charge">
+                        <option value="klien">nama klien</option>
+                    </select>
                   </div>
               </div> 
               <div class="row pl-5 pr-5" style="margin-top: 5px;">
                   <div class="col-md-4">
-                      <p>Tanggal</p>
+                      <p>ID Transaksi</p>
                   </div>
                   <div class="col-md-8">
-                    <input type="number" class="form-control" name="total" placeholder="Harga" value="{{old('harga')}}">
+                    <select class="form-control select2" name="id" placeholder="Pilih ID transaksi">
+                        <option value="asesmen">id transaksi</option>
+                    </select>
                   </div>
-              </div>                                           
+              </div>                                          
             </div>
             <div class="col-md-6 pl-5 pr-5">
               <div class="row pl-5 pr-5" style="margin-top: 5px;">
@@ -57,21 +62,24 @@
                       <p>Total</p>
                   </div>
                   <div class="col-md-8">
-                    <input type="number" class="form-control" name="harga" placeholder="Harga" value="{{old('harga')}}">
+                    <input type="number" class="form-control" name="harga" placeholder="Total" value="{{old('harga')}}">
                   </div>
               </div> 
             </div>
           </div>   
         </div>
+
         <div class="box box-default" style="padding-top: 30px; ">
           <div class="box-body">
-          <div class="col-md-12">
+            <div class="col-md-12" style="margin-bottom:50px;" >
               <div class="col-md-3" style="margin-top: 5px;">
                   <div class="col-md-4">
                       <p>ID</p>
                   </div>
                   <div class="col-md-8">
-                    <input type="text" class="form-control" name="nama" placeholder="Asesmen" value="{{old('nama')}}">
+                    <select class="form-control select2" name="id" placeholder="Pilih ID">
+                        <option value="id">1</option>
+                    </select>
                   </div>
               </div>
               <div class="col-md-4" style="margin-top: 5px;">
@@ -79,7 +87,9 @@
                       <p>Asesmen & Charge</p>
                   </div>
                   <div class="col-md-6">
-                    <input type="text" class="form-control" name="nama" placeholder="Asesmen" value="{{old('nama')}}">
+                     <select class="form-control select2" name="asesmen" placeholder="Pilih Asesmen/Charge">
+                        <option value="asesmen">asesmen</option>
+                    </select>
                   </div>
               </div>
               <div class="col-md-3" style="margin-top: 5px;">
@@ -87,38 +97,78 @@
                       <p>Harga</p>
                   </div>
                   <div class="col-md-8">
-                    <input type="text" class="form-control" name="nama" placeholder="Asesmen" value="{{old('nama')}}">
+                    <input type="text" class="form-control" name="nama" placeholder="harga" value="{{old('nama')}}">
                   </div>
               </div>
               <div class="col-md-2" style="margin-top: 5px;">
-                  <button type="button" class="btn btn-info" onclick="location.href='{{url('data/asesmen')}}'">Cari
+                  <button type="button" class="btn btn-info" onclick="location.href='{{url('data/asesmen')}}'">Tambah
                   </button>
               </div>
             </div>
-          </div>   
+
+            <table id="datatable1" class="table table-bordered table-striped" >
+              <thead>
+                <tr>
+                  <th class="text-center">ID</th>
+                  <th class="text-center">LAYANAN</th>
+                  <th class="text-center" width="300px;">ASESMEN/CHARGE</th>
+                  <th class="text-center">HARGA</th>
+                  <th class="text-center" class="wd-10p">ACTION</th>
+                </tr>
+              </thead>
+              <tbody>                        
+                  <tr>
+                      <td align="center">1</td>
+                      <td>nama</td>
+                      <td>layanan</td>
+                      <td>harga</td>
+                      <td>
+                        <button type="button" class="btn btn-danger" onclick="location.href='{{url('data/asesmen')}}'">Hapus
+                    </button>
+                      </td>
+                  </tr>                                                            
+              </tbody>
+            </table>
+          </div>
         </div>
 
         <div class="box box-default" style="padding-top: 30px; ">
-          <table id="datatable1" class="table table-bordered table-striped" >
-            <thead>
-              <tr>
-                <th class="text-center">ID</th>
-                <th class="text-center">LAYANAN</th>
-                <th class="text-center" width="300px;">ASESMEN/CHARGE</th>
-                <th class="text-center">HARGA</th>
-                <th class="text-center" class="wd-10p">ACTION</th>
-              </tr>
-            </thead>
-            <tbody>                        
-                <tr>
-                    <td align="center">1</td>
-                    <td>nama</td>
-                    <td>layanan</td>
-                    <td>harga</td>
-                    <td>x</td>
-                </tr>                                                            
-            </tbody>
-          </table>
+          <div class="box-body">
+            <div class="col-md-6 pl-5 pr-5" style="float:left">
+              <div class="row pl-5 pr-5" style="margin-top: 5px;">
+                  <div class="col-md-4">
+                      <p>Total</p>
+                  </div>
+                  <div class="col-md-8">
+                    <input type="number" class="form-control" name="total" placeholder="total" value="{{old('harga')}}">
+                  </div>
+              </div> 
+              <div class="row pl-5 pr-5" style="margin-top: 5px;">
+                  <div class="col-md-4">
+                      <p>Bayar</p>
+                  </div>
+                  <div class="col-md-8">
+                    <input type="number" class="form-control" name="total" placeholder="bayar" value="{{old('harga')}}">
+                  </div>
+              </div> 
+              <div class="row pl-5 pr-5" style="margin-top: 5px;">
+                  <div class="col-md-4">
+                      <p>Kembalian</p>
+                  </div>
+                  <div class="col-md-8">
+                    <input type="number" class="form-control" name="total" placeholder="kembalian" value="{{old('kembalian')}}">
+                  </div>
+              </div>                                          
+            </div>
+            <div class="col-md-12 pl-5 pr-5">
+              <div class="row pl-5 pr-5" style="margin-top: 5px; padding-left: 10px;">
+                  <button type="button" class="btn btn-danger" onclick="location.href='{{url('data/asesmen')}}'">Batal
+                  </button>
+                 <button type="button" class="btn btn-success" onclick="location.href='{{url('data/asesmen')}}'">Simpan
+                  </button>
+              </div> 
+            </div>
+          </div>   
         </div>
 
     </section>
